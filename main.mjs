dff851e8f55f7f37b04fafed5f0302d6e8fa1a44
Felquis Gimenes <frgformenton@gmail.com>
2020-12-01 23:28:59 -0300

    Updates

--- main.mjs
+++ main.mjs
@@ -1,7 +1,7 @@
 import './App.js'
-import colorName from './es-third-party/es-color-name/index.js'
+import colorName from './es-color-name.js'
 
-document.body.style.backgroundColor = `rgb(${colorName.aliceblue.join(',')})`
+document.body.style.backgroundColor = `rgb(${colorName.khaki.join(',')})`
 
 document.addEventListener('DOMContentLoaded', () =>{
     const element = document.createElement('x-app')
